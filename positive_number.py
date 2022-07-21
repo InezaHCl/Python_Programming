f54dc818a454f8ffae9fa977943512d551d7dafc
HERVE CLAUDEL <99665264+InezaHCl@users.noreply.github.com>
2022-07-21 13:59:58 +0530

    first attempt

---
+++ positive_number.py
@@ -0,0 +1,7 @@
+#Creating the list
+myList = [12, -1, 3.5, -21, 0.91]
+#Checking the number is positive or not and print the value
+
+for element in myList:
+    if element >= 0:
+        print(element)
