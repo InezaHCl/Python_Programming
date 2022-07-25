f977c9d2e31ba65ae27198b1ff7cd19afcb06d0e
HERVE CLAUDEL <99665264+InezaHCl@users.noreply.github.com>
2022-07-25 09:46:00 +0530

    Done1

---
+++ Print Letters_From_string.py
@@ -0,0 +1,24 @@
+#Python code to create a function called most_frequent that takes a string 
+#And prints the letters in decreasing order of frequency. Use dictionaries.
+
+text = 'Mississippi'
+
+
+def make_dict(x):
+    dictionary = {}
+    for letter in x:
+        dictionary[letter] = 1 + dictionary.get(letter, 0)
+    return dictionary
+
+
+def most_frequent(text):
+    letters = [letter.lower() for letter in text if letter.isalpha()]
+    dictionary = make_dict(letters)
+    result = []
+    for key in dictionary:
+        result.append((dictionary[key], key))
+    result.sort(reverse=True)
+    for count, letter in result:
+        print(letter, count)
+
+most_frequent(text)
