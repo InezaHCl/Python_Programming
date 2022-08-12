e00e9fbda2013de3947f5131bb7658b7487b74b2
HERVE CLAUDEL <99665264+InezaHCl@users.noreply.github.com>
2022-08-12 10:59:19 +0530

    done_2
    added function call and also input() function to take input from the user

--- counting.py
+++ counting.py
@@ -3,10 +3,13 @@ Python program to counts the number of times the letter appears in a string usin
 """
 
 #counting how many times the letter "a" appears in a string
+def count(Str):
+    val = input('Enter a letter you want to count: ')
+    count = 0  #The variable count is initialized to 0
+    for letter in Str:
+        if letter == val:
+            count = count + 1  #The variable count is incremented each time the letter you want is found
+    return count  #The return statement send back the the computed value back to the calling code
 
-word = "banana"
-count = 0  #The variable count is initialized to 0
-for letter in word:
-    if letter == 'a':
-        count = count + 1  # the variable count is incremented each time an "a" is found
-print(count)  #When the loop exits, count contains the results
+Str = input('Enter a string: ')  #Asking the user to input the string
+count(Str) #function call
